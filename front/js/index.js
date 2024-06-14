99a347b733a4313e7e56940859b736f88c559d30
MOISES-DARLAN <moisesdarlan440@gmail.com>
2024-06-14 19:34:56 -0300

    criado section sobre mim

---
+++ front/js/index.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({reset: true})
+
+
+// sr.reveal('#home', {duration: 1000, rotate:{x:0, y:80, z:0}
+// })
+sr.reveal('.invisible', {duration: 1000, rotate:{x:100, y:-100, z:100}
+})
+sr.reveal('.area-1', {duration: 2000, rotate:{x:0, y:40, z:0}})
+
